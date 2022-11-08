05cfea2ad33edf0716a26cf16f3e16bfa6807dce
mohamed <mouh4945@gmail.com>
2022-11-08 03:14:38 -0800

    ADD

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,11 +1,36 @@
 import Image from "next/image"
+import { useState } from "react"
 export default function Navbar() {
+
+     const [isHovered , setIsHovered] = useState(false);
+     
+     const hoverHandler = () => {
+        setIsHovered( () => {
+            if(isHovered == false) {
+                return true
+            } else {
+                return false
+            }
+        } )
+        console.log(isHovered)
+     }
+
     return (
         <navbar className=''>
            <div className="flex justify-between items-center px-10   py-5">
            <div> <Image src={'/logo.png'} width={120} height={60} alt={'logo-Image'} /> </div>
-            <div> <Image src={'/MenuIcon.png'} width={20} height={43} alt={'iconMenu'} /></div>
+           <button
+            onClick={hoverHandler}
+           className="hover:bg-blue-400" >
+          <Image src={'/MenuIcon.png'} width={20} height={43} alt={'iconMenu'} />
+           </button>
+
+         
            </div>
+
+           <div className="bg-blue-400 h-[20rem]"> 
+            hello
+            </div>
         </navbar>
     )   
 }
