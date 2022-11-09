9b97dcda88fd90cfbb19b9a8b94d5e62067fef6e
mohamed <mouh4945@gmail.com>
2022-11-09 13:36:38 -0800

    ADD

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,36 +1,73 @@
-import Image from "next/image"
-import { useState } from "react"
+import Image from "next/image";
+import { useState , useRef } from "react";
 export default function Navbar() {
+  const [isHovered, setIsHovered] = useState(false);
 
-     const [isHovered , setIsHovered] = useState(false);
-     
-     const hoverHandler = () => {
-        setIsHovered( () => {
-            if(isHovered == false) {
-                return true
-            } else {
-                return false
-            }
-        } )
-        console.log(isHovered)
-     }
+  const innerHTMLContainer = useRef();
+  const hoverHandler = () => {
+    setIsHovered(() => {
+      if (isHovered == false) {
+        return true;
+      } else {
+        return false;
+      }
+    });
+    console.log(isHovered);
+  };
 
-    return (
-        <navbar className=''>
-           <div className="flex justify-between items-center px-10   py-5">
-           <div> <Image src={'/logo.png'} width={120} height={60} alt={'logo-Image'} /> </div>
-           <button
-            onClick={hoverHandler}
-           className="hover:bg-blue-400" >
-          <Image src={'/MenuIcon.png'} width={20} height={43} alt={'iconMenu'} />
-           </button>
 
-         
-           </div>
+ if(isHovered == false) {
+   console.log(innerHTMLContainer.current)
+  //addEventListener( 'click ' , () => console.log('hello'))
+ }
 
-           <div className="bg-blue-400 h-[20rem]"> 
-            hello
-            </div>
-        </navbar>
-    )   
+
+const listName = [1 , 3.4]; 
+console.log(listName.map( (e) => Math.SQRT1_2))
+
+  return (
+    <navbar className="">
+      <div className="flex justify-between items-center px-10   py-5">
+        <div>
+          {" "}
+          <Image
+            src={"/logo.png"}
+            width={120}
+            height={60}
+            alt={"logo-Image"}
+          />{" "}
+        </div>
+        <button onClick={hoverHandler} className="hover:bg-blue-400">
+          <Image
+            src={"/MenuIcon.png"}
+            width={20}
+            height={43}
+            alt={"iconMenu"}
+          />
+        </button>
+      </div>
+
+      { isHovered ?  
+      <div ref={innerHTMLContainer} className="bg-primary  border-r-2 border-primary flex items-center justify-center flex-col py-10 absolute top-0 left-0 w-2/4 z-10 h-full ">
+  <ul className="flex justify-center relative flex-col   items-center space-y-10   mt-2 " >
+    <li key={1} className='font-medium text-2xl hover:text-4xl hover:transition-all transition-transform capitalize text-white '> home</li>
+    <li key={1} className='font-normal text-2xl capitalize text-white '>the product</li>
+    <li key={1} className='font-normal text-2xl capitalize text-white '> customers </li>
+    <li key={1}className='font-normal text-2xl capitalize text-white '> contact us </li>
+  </ul>
+
+  {/* animations icons */}
+  <div className="  animate-spin animate-ping bg-white w-10 h-10  absolute  top-20 left-20">
+    <Image src={''} alt={''} width={''} height={''} />
+  </div>
+
+  <div className="  animate-bounce bg-white/40 w-10 h-10  absolute  bottom-24 right-40">
+    <Image src={''} alt={''} width={''} height={''} />
+  </div>
+
+</div>:''}
+
+    
+    </navbar>
+  );
 }
